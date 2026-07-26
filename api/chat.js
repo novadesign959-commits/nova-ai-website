2d7929322852f84844a9a52db06a84520228ae26
novadesign959-commits <novadesign959@gmail.com>
2026-07-27 00:12:04 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -18,7 +18,12 @@ export default async function handler(req, res) {
 
   const data = await response.json();
 
-  res.status(200).json({
-    answer: data.choices[0].message.content
+if (!response.ok) {
+  return res.status(500).json({
+    answer: JSON.stringify(data)
   });
-    }
+}
+
+res.status(200).json({
+  answer: data.choices[0].message.content
+});
